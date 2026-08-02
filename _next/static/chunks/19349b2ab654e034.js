d0d6c7e31c247bd8f50a7ff186589db0a213ff91
necto-cms <necto-cms@users.noreply.github.com>
2026-08-02 20:29:49 +0000

    deploy: necto-cms/new-site-xto@2c10c00136ea663485bf5cd4ae2f98207e9ab8a7

--- _next/static/chunks/19349b2ab654e034.js
+++ _next/static/chunks/19349b2ab654e034.js
@@ -1,10 +1,10 @@
 __turbopack_load_page_chunks__("/_app", [
-  "static/chunks/b1ed81e92bd75fc9.js",
+  "static/chunks/29d42e0da65ce501.js",
   "static/chunks/33f493a1ac456adb.js",
   "static/chunks/4f5a902b8d8095a5.js",
   "static/chunks/1929ed94aa2132a3.js",
   "static/chunks/db5db5b2128639ef.js",
   "static/chunks/33199af12bee9778.js",
   "static/chunks/c5896ec53059be3b.css",
-  "static/chunks/turbopack-28adeaabada126da.js"
+  "static/chunks/turbopack-ea723cbf61b087b4.js"
 ])
