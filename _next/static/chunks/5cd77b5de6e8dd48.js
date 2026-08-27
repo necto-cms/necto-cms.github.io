bd588b95831c91290d68288e5692f985eed194da
necto-cms <necto-cms@users.noreply.github.com>
2026-08-27 20:36:41 +0000

    deploy: necto-cms/new-site-xto@a01961305a4570051caee1b3b7bcd6e2e2df5951

--- _next/static/chunks/5cd77b5de6e8dd48.js
+++ _next/static/chunks/5cd77b5de6e8dd48.js
@@ -1,7 +1,7 @@
 __turbopack_load_page_chunks__("/tools/device-bridge", [
-  "static/chunks/a3e5fff102cb17d1.js",
+  "static/chunks/44380081b38687aa.js",
   "static/chunks/33f493a1ac456adb.js",
   "static/chunks/db5db5b2128639ef.js",
   "static/chunks/33199af12bee9778.js",
-  "static/chunks/turbopack-ad558733a7d312e5.js"
+  "static/chunks/turbopack-1c292c2eeec4d2ee.js"
 ])
--- _next/static/v1GeScfa2pU_NvteqMZI9/_buildManifest.js
+++ _next/static/v1GeScfa2pU_NvteqMZI9/_buildManifest.js
@@ -27,7 +27,7 @@ self.__BUILD_MANIFEST = {
     "static/chunks/3cb6f4e53e320c9a.js"
   ],
   "/tools/device-bridge": [
-    "static/chunks/d01ff1ea0712f40d.js"
+    "static/chunks/5cd77b5de6e8dd48.js"
   ],
   "/tools/docker-builder": [
     "static/chunks/c46b8735d887a3eb.js"
